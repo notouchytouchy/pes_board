cebde60758479547b5a9bbdcca8a1aee8c47ccc7
Patrick <patrick.tenger@gmx.ch>
2026-03-25 15:16:50 +0100

    changes for Servo

--- src/move_servo.cpp
+++ src/move_servo.cpp
@@ -9,30 +9,36 @@
 
 
 bool move_servo(int color_to_servo){ // wert color_to_sensor: 0 = no input, 3 = red, 4 = yello, 5 = green, 7 = blue
-    StateTimer transition_timer; // initialising timer funktion
-    int last_state = 0; // remembers the state active befor the current state
+    static StateTimer transition_timer; // initialising timer funktion
+    static bool initialized=false;
+
 
     // servo_DO = S-0090, servo_D1 = S-3003
-    Servo servo_D0(PB_D0);
-    Servo servo_D1(PB_D1);
+    static Servo servo_D0(PB_D0);
+    static Servo servo_D1(PB_D1);
 
     // minimal pulse width and maximal pulse width obtained from the servo calibration process
     // reely S0090
-    float servo_D0_ang_min = 0.0325f; // careful, these values might differ from servo to servo
-    float servo_D0_ang_max = 0.1250f;
+    static float servo_D0_ang_min = 0.0325f; // careful, these values might differ from servo to servo
+    static float servo_D0_ang_max = 0.1250f;
     // futuba S3001
-    float servo_D1_ang_min = 0.0325f;
-    float servo_D1_ang_max = 0.1300f;
+    static float servo_D1_ang_min = 0.0325f;
+    static float servo_D1_ang_max = 0.1300f;
     // servo.setPulseWidth: before calibration (0,1) -> (min pwm, max pwm)
-    // servo.setPulseWidth: after calibration (0,1) -> (servo_D0_ang_min, servo_D0_ang_max)
-    servo_D0.calibratePulseMinMax(servo_D0_ang_min, servo_D0_ang_max);
-    servo_D1.calibratePulseMinMax(servo_D1_ang_min, servo_D1_ang_max);
-    // default acceleration of the servo motion profile is 1.0e6f
-    servo_D0.setMaxAcceleration(1.0e4f);
-    servo_D1.setMaxAcceleration(1.0e4f);
 
-    float servo_input_D0 = 0.0f;
-    float servo_input_D1 = 0.0f;
+    if(initialized == false){
+        // servo.setPulseWidth: after calibration (0,1) -> (servo_D0_ang_min, servo_D0_ang_max)
+        servo_D0.calibratePulseMinMax(servo_D0_ang_min, servo_D0_ang_max);
+        servo_D1.calibratePulseMinMax(servo_D1_ang_min, servo_D1_ang_max);
+        // default acceleration of the servo motion profile is 1.0e6f
+        servo_D0.setMaxAcceleration(0.3f);
+        servo_D1.setMaxAcceleration(0.3f);
+
+        initialized = true;
+    }
+    static float servo_input_D0 = 1.0f;
+    static float servo_input_D1 = 1.0f;
+
 
     // set up states for state machine
     enum ServoState {
@@ -41,28 +47,30 @@ bool move_servo(int color_to_servo){ // wert color_to_sensor: 0 = no input, 3 = 
         ROTATE_IN,
         SLEEP,
         FINISHED
-    } servo_state = ServoState::INITIAL;
+    } static servo_state = ServoState::INITIAL;
+    static ServoState last_state = ServoState::ROTATE_OUT; // remembers the state active befor the current state
 
-    if (servo_state == ServoState::INITIAL && (color_to_servo == 3 || color_to_servo == 4 || color_to_servo == 5 || color_to_servo == 7)){
-                servo_state = ServoState::ROTATE_OUT;
-            }
 
     // state machine
     switch (servo_state) {
         case ServoState::INITIAL: {
-            if (servo_D0.isEnabled()){
-                servo_D0.disable();
+            printf("Initial");
+            if (!servo_D0.isEnabled()){
+                servo_D0.enable(1.0f);
             }
-            last_state = ServoState::INITIAL;
+            
+            
+            if (servo_state == ServoState::INITIAL && (color_to_servo == 3 || color_to_servo == 4 || color_to_servo == 5 || color_to_servo == 7)){
+                servo_state = ServoState::ROTATE_OUT;
+            }
 
             return false;
         }
         case ServoState::ROTATE_OUT: {
-            if (!servo_D0.isEnabled()){
-                servo_D0.enable();
-            }
+            printf("Rotate OUT\n");
+            
             // function to map the distance to the servo movement -> (0.0f, 1.0f)
-            servo_input_D0 = 1.0f;
+            servo_input_D0 = 0.3f;
             // values smaller than 0.0f or bigger than 1.0f are constrained to the range (0.0f, 1.0f) in setPulseWidth
             servo_D0.setPulseWidth(servo_input_D0);
             transition_timer.start(3000);
@@ -73,8 +81,9 @@ bool move_servo(int color_to_servo){ // wert color_to_sensor: 0 = no input, 3 = 
             break;
         }
         case ServoState::ROTATE_IN: {
+            printf("Rotate IN\n");
             // function to map the distance to the servo movement -> (0.0f, 1.0f)
-            servo_input_D0 = 0.0f;
+            servo_input_D0 = 1.0f;
             // values smaller than 0.0f or bigger than 1.0f are constrained to the range (0.0f, 1.0f) in setPulseWidth
             servo_D0.setPulseWidth(servo_input_D0);
             transition_timer.start(3000);
@@ -85,22 +94,31 @@ bool move_servo(int color_to_servo){ // wert color_to_sensor: 0 = no input, 3 = 
             break;
         }
         case ServoState::SLEEP: {
+            printf("Sleep\n");
             if (transition_timer.isExpired()) {
+                printf("timer abgelaufen\n");
                 if (last_state == ServoState::ROTATE_OUT){
                     servo_state = ServoState::ROTATE_IN;
+                  printf("sleep1");
                 }
                 else if (last_state == ServoState::ROTATE_IN) {
                     servo_state = ServoState::FINISHED;
+                    printf("sleep2");
+                  
                 }
-            }
-            last_state = ServoState::SLEEP;
 
+                
+            }
+            
+            printf("%d", last_state);
             break;
         }
         case ServoState::FINISHED: {
+            printf("Finished");
             servo_state = ServoState::INITIAL;
 
             return true; // signal to main, that the task is done
+            
         }
         default: {
 
