c73fa09879e4ad1320a951da6d967977e4dece5a
Temp Main Dateien für Periferieen <lukas.vonmassenbach@gmail.com>
2026-04-22 13:37:09 +0200

    Servo endvalues have been changed

--- src/move_servo.cpp
+++ src/move_servo.cpp
@@ -24,43 +24,19 @@ bool move_servo(int color_to_servo, int packageReceived){ // wert color_to_senso
     // minimal pulse width and maximal pulse width obtained from the servo calibration process
     // reely S0090
     static float servo_D1_ang_min = 0.0325f; // careful, these values might differ from servo to servo
-    static float servo_D1_ang_max = 0.1250f;
+    static float servo_D1_ang_max = 0.1300f;
     // futuba S3001
     static float servo_D0_ang_min = 0.0325f;
     static float servo_D0_ang_max = 0.1300f;
     // futuba S3001
     static float servo_D2_ang_min = 0.0325f;
-    static float servo_D2_ang_max = 0.1300f;
+    static float servo_D2_ang_max = 0.1250f;
     // futuba S3001
     static float servo_D3_ang_min = 0.0325f;
     static float servo_D3_ang_max = 0.1300f;
     // servo.setPulseWidth: before calibration (0,1) -> (min pwm, max pwm)
 
     if(initializedReceiving == false && packageReceived<4){ // initializing servos for receiving the packages
-        // servo.setPulseWidth: after calibration (0,1) -> (servo_D0_ang_min, servo_D0_ang_max)
-        servo_D0.calibratePulseMinMax(servo_D0_ang_min, servo_D0_ang_max);
-        servo_D1.calibratePulseMinMax(servo_D1_ang_min, servo_D1_ang_max);
-        servo_D2.calibratePulseMinMax(servo_D2_ang_min, servo_D2_ang_max);
-        servo_D3.calibratePulseMinMax(servo_D3_ang_min, servo_D3_ang_max);
-        // default acceleration of the servo motion profile is 1.0e6f
-        servo_D0.setMaxAcceleration(0.3f);
-        servo_D1.setMaxAcceleration(0.3f);
-        servo_D2.setMaxAcceleration(0.3f);
-        servo_D3.setMaxAcceleration(0.3f);
-        // check to make sure servo outputs are disabled bevor start
-        if(servo_D0.isEnabled())
-            servo_D0.disable();
-        if(servo_D1.isEnabled())
-            servo_D1.disable();
-        if(servo_D2.isEnabled())
-            servo_D2.disable();
-        if(servo_D3.isEnabled())
-            servo_D3.disable();
-
-        initializedReceiving = true;
-    }
-
-    if(initializedDelievering == false && packageReceived==4){ // initializing servos for delievering the packages
         // servo.setPulseWidth: after calibration (0,1) -> (servo_D0_ang_min, servo_D0_ang_max)
         servo_D0.calibratePulseMinMax(servo_D0_ang_min, servo_D0_ang_max);
         servo_D1.calibratePulseMinMax(servo_D1_ang_min, servo_D1_ang_max);
@@ -81,11 +57,35 @@ bool move_servo(int color_to_servo, int packageReceived){ // wert color_to_senso
         if(servo_D3.isEnabled())
             servo_D3.disable();
 
+        initializedReceiving = true;
+    }
+
+    if(initializedDelievering == false && packageReceived==4){ // initializing servos for delievering the packages
+        // servo.setPulseWidth: after calibration (0,1) -> (servo_D0_ang_min, servo_D0_ang_max)
+        servo_D0.calibratePulseMinMax(servo_D0_ang_min, servo_D0_ang_max);
+        servo_D1.calibratePulseMinMax(servo_D1_ang_min, servo_D1_ang_max);
+        servo_D2.calibratePulseMinMax(servo_D2_ang_min, servo_D2_ang_max);
+        servo_D3.calibratePulseMinMax(servo_D3_ang_min, servo_D3_ang_max);
+        // default acceleration of the servo motion profile is 1.0e6f
+        servo_D0.setMaxAcceleration(0.2f);
+        servo_D1.setMaxAcceleration(0.2f);
+        servo_D2.setMaxAcceleration(0.2f);
+        servo_D3.setMaxAcceleration(0.2f);
+        // check to make sure servo outputs are disabled bevor start
+        if(servo_D0.isEnabled())
+            servo_D0.disable();
+        if(servo_D1.isEnabled())
+            servo_D1.disable();
+        if(servo_D2.isEnabled())
+            servo_D2.disable();
+        if(servo_D3.isEnabled())
+            servo_D3.disable();
+
         initializedDelievering = true;
     }
-    static float servo_input_D0 = 1.0f;
+    static float servo_input_D0 = 0.0f;
     static float servo_input_D1 = 1.0f;
-    static float servo_input_D2 = 1.0f;
+    static float servo_input_D2 = 0.0f;
     static float servo_input_D3 = 1.0f;
 
 
@@ -111,28 +111,28 @@ bool move_servo(int color_to_servo, int packageReceived){ // wert color_to_senso
         case ServoState::ROTATE_OUT: { // move robot out
            
             if(color_to_servo == 4){ //yellow servo
-                servo_D0.enable(1.0f);
-                servo_input_D0 = 0.43f;
+                servo_D0.enable(0.0f);
+                servo_input_D0 = 0.5f;
                 servo_D0.setPulseWidth(servo_input_D0);
             }
             else if(color_to_servo == 3){ //red servo
-                servo_D1.enable(1.0f);
-                servo_input_D1 = 0.43f;
+                servo_D1.enable(0.98f);
+                servo_input_D1 = 0.5f;
                 servo_D1.setPulseWidth(servo_input_D1);
             }
             else if(color_to_servo == 7){ //blue servo
-                servo_D2.enable(1.0f);
-                servo_input_D2 = 0.43;
+                servo_D2.enable(0.0f);
+                servo_input_D2 = 0.5f;
                 servo_D2.setPulseWidth(servo_input_D2);
             }
             else if(color_to_servo == 5){ //green servo
                 servo_D3.enable(1.0f);
-                servo_input_D3 = 0.43f;
+                servo_input_D3 = 0.5f;
                 servo_D3.setPulseWidth(servo_input_D3);
             }
             else printf("No usable colortype detected");
 
-            transition_timer.start(2500);
+            transition_timer.start(3000);
             servo_state = ServoState::SLEEP_OUT;
 
             break;
@@ -148,24 +148,24 @@ bool move_servo(int color_to_servo, int packageReceived){ // wert color_to_senso
         case ServoState::ROTATE_IN: { // move robot back in
             
             if(color_to_servo == 4){ //yello servo
-                servo_input_D0 = 1.0f;
+                servo_input_D0 = 0.02f;
                 servo_D0.setPulseWidth(servo_input_D0);
             }
             else if(color_to_servo == 3){ //red servo
-                servo_input_D1 = 1.0f;
+                servo_input_D1 = 0.98f;
                 servo_D1.setPulseWidth(servo_input_D1);
             }
             else if(color_to_servo == 7){ //blue servo
-                servo_input_D2 = 1.0f;
+                servo_input_D2 = 0.02f;
                 servo_D2.setPulseWidth(servo_input_D2);
             }
             else if(color_to_servo == 5){ //green servo
-                servo_input_D3 = 1.0f;
+                servo_input_D3 = 0.98f;
                 servo_D3.setPulseWidth(servo_input_D3);
             }
             else printf("No usable colortype detected");
 
-            transition_timer.start(2500);
+            transition_timer.start(3000);
             servo_state = ServoState::SLEEP_IN;
 
             break;
